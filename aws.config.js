9bb54405caf5905624b34de9d73d35252f9ab4ba
samueljames <samuel4abiodun@gmail.com>
2020-02-23 20:03:55 +0100

    update aws config

--- aws.config.js
+++ aws.config.js
@@ -1,14 +1,14 @@
 import { Auth } from "aws-amplify";
 export default {
   Auth: {
-    identityPoolId: 'eu-west-1:53b76f45-ac17-452e-a012-a8d9dcfd9160',
+    identityPoolId: 'XXXXX',
     region: 'eu-west-1',
-    userPoolId: 'eu-west-1_p19Y22Ov5',
-    userPoolWebClientId: '3ia2e8h2g8309mgbn2l6ushuqk',
+    userPoolId: 'XXXXX',
+    userPoolWebClientId: 'XXXXX',
   },
   Storage: {
     AWSS3: {
-      bucket: 'gram-to-store-images', //REQUIRED -  Amazon S3 bucket
+      bucket: 'XXXXXX', //REQUIRED -  Amazon S3 bucket
       region: 'eu-west-1', //OPTIONAL -  Amazon service region
     }
   }
